69a436fcac34994c7059ebd6e127029f23bec3d9
KauanSarzi <kauansarzi57@gmail.com>
2024-10-25 22:26:46 -0300

    Add files via upload

---
+++ POC5/src/POC 5/src/app/page.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Button from '../components/Button';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>Bem-vindo à minha aplicação Next.js!</p>
+      <Button />
+    </main>
+  );
+}
